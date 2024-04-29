c1c843df34d6da770cd56dbbdbd34b6207508215
Your Name <you@example.com>
2024-04-29 00:34:11 -0700

    all finished

--- CS564/P6 Minirel Query Operator/part6/delete.C
+++ CS564/P6 Minirel Query Operator/part6/delete.C
@@ -1,6 +1,6 @@
+#include <stdlib.h>
 #include "catalog.h"
 #include "query.h"
-
 
 /*
  * Deletes records from a specified relation.
@@ -10,17 +10,112 @@
  * 	an error code otherwise
  */
 
-const Status QU_Delete(const string & relation, 
-		       const string & attrName, 
-		       const Operator op,
-		       const Datatype type, 
-		       const char *attrValue)
+const Status QU_Delete(const string &relation,
+                       const string &attrName,
+                       const Operator op,
+                       const Datatype type,
+                       const char *attrValue)
+
 {
-// part 6
-return OK;
 
+  // check if relation table is empty then proceed
+  if (relation.empty())
+  {
+    return BADCATPARM;
+  }
 
+  Status status;
+  RID rid;
+  AttrDesc attrD;
+  HeapFileScan *scanner;
 
+  // Where clause is empty
+  if (attrName.empty())
+  {
+
+    // creating scanner object
+    scanner = new HeapFileScan(relation, status);
+    if (status != OK)
+    {
+      return status;
+    }
+
+    // start the scan
+    scanner->startScan(0, 0, STRING, NULL, EQ);
+    // iterate and call next in scanner object to go over the results and deleting the records
+    while ((status = scanner->scanNext(rid)) != FILEEOF)
+    {
+      status = scanner->deleteRecord();
+      if (status != OK)
+      {
+        return status;
+      }
+    }
+  }
+
+  else
+  {
+
+    // creating Scanner object
+    scanner = new HeapFileScan(relation, status);
+    if (status != OK)
+    {
+      return status;
+    }
+
+    // getting information about attribute
+    status = attrCat->getInfo(relation, attrName, attrD);
+    if (status != OK)
+    {
+      return status;
+    }
+
+    // Switch which starts scan based on data type
+    int intVal;
+    float floatVal;
+    switch (type)
+    {
+    case INTEGER:
+
+      intVal = atoi(attrValue);
+      status = scanner->startScan(attrD.attrOffset, attrD.attrLen, type, (char *)&intVal, op);
+      break;
+
+    case FLOAT:
+      floatVal = atof(attrValue);
+      status = scanner->startScan(attrD.attrOffset, attrD.attrLen, type, (char *)&floatVal, op);
+      break;
+
+      // default condition i.e string
+    default:
+      status = scanner->startScan(attrD.attrOffset, attrD.attrLen, type, attrValue, op);
+      break;
+    }
+
+    if (status != OK)
+    {
+      return status;
+    }
+
+    // iterate and call next in scanner object to go over the results and deleting the records
+    while ((status = scanner->scanNext(rid)) == OK)
+    {
+      status = scanner->deleteRecord();
+      if (status != OK)
+      {
+        return status;
+      }
+    }
+  }
+
+  if (status != FILEEOF)
+  {
+    return status;
+  }
+
+  // scan finished
+  scanner->endScan();
+  delete scanner;
+
+  return OK;
 }
-
-
--- CS564/P6 Minirel Query Operator/part6/temp.C
+++ CS564/P6 Minirel Query Operator/part6/temp.C
@@ -155,7 +155,173 @@ const Status ScanSelect(const string &result,
 		// Store the record in the result file
 		if ((status = resultRel.insertRecord(outputRec, outRID)) != OK){
 			return status;
-		}		
+		}		#include "catalog.h"
+#include "query.h"
+
+
+// forward declaration
+const Status ScanSelect(const string & result, 
+			const int projCnt, 
+			const AttrDesc projNames[],
+			const AttrDesc *attrDesc, 
+			const Operator op, 
+			const char *filter,
+			const int reclen);
+
+
+const Status QU_Select(const string & result, 
+		       const int projCnt, 
+		       const attrInfo projNames[],
+		       const attrInfo *attr, 
+		       const Operator op, 
+		       const char *attrValue)
+{
+    // Qu_Select sets up things and then calls ScanSelect to do the actual work
+    cout << "Doing QU_Select " << endl;
+
+
+
+    //1. Check if the input has a valid attribute type, we only have 0,1,2
+    if ((attr != NULL) && (attr->attrType < 0 || attr->attrType > 2)){
+		return BADCATPARM;
+	}
+
+
+    //2. Consult the catalog to go from attrInfo to attrDesc (attrCat and relCat, global variables)	    
+	Status status;
+    AttrDesc *attrDesc;
+    int attribute_Count;
+    if ((status = attrCat->getRelInfo(projNames[0].relName, attribute_Count, attrDesc)) != OK){
+		return status;
+	}
+
+
+
+	// 3. GO through the projection list and look up each in the attr catalog to get an AttrDesc structure
+	int record_length = 0;
+	AttrDesc projoect_AttrDesc[projCnt];
+	for (int projIndex = 0; projIndex < projCnt; projIndex++){
+		for (int attrIndex = 0; attrIndex < attribute_Count; attrIndex++){
+			// Check if the attribute is in the projection list
+			if (strcmp(attrDesc[attrIndex].attrName, projNames[projIndex].attrName) == 0)
+			{
+				// Attribute found in the projection list
+				// 1) Update record length
+				record_length += attrDesc[attrIndex].attrLen;
+				// 2) Populate projection attribute description
+				projoect_AttrDesc[projIndex] = attrDesc[attrIndex];
+				break; // Move to the next attribute in the projection list
+			}
+		}
+	}
+
+	// 4. Find the index of search attribute if specified
+	int specified_attr_index  = 0;
+	if (attr != NULL){
+		for (int attrIndex = 0; attrIndex < attribute_Count; attrIndex++){
+			if (strcmp(attrDesc[attrIndex].attrName, attr->attrName) == 0){
+				// Search attribute found
+				specified_attr_index = attrIndex;
+				break; // No need to continue searching, end loop
+			}
+		}
+	}
+
+    // 5. Call up ScanSelect to execute work
+    if (attr == NULL)
+        return ScanSelect(result, projCnt, projoect_AttrDesc, &projoect_AttrDesc[0], EQ, NULL, record_length);
+    else
+        return ScanSelect(result, projCnt, projoect_AttrDesc, &attrDesc[specified_attr_index], op, attrValue, record_length);
+}
+
+
+const Status ScanSelect(const string & result, 
+			const int projCnt,
+			const AttrDesc projNames[],
+			const AttrDesc *attrDesc, 
+			const Operator op, 
+			const char *filter,
+			const int reclen)
+{
+    cout << "Doing HeapFileScan Selection using ScanSelect()" << endl;
+
+	// 1. Have a temporary record for output table
+	char outputData[reclen];
+
+	// 2. Open "result" as an InsertFileScan object
+	Status status;
+    InsertFileScan resultFile(result, status);
+    if (status != OK){
+        return status;
+	}
+
+	// 3. Open current table (to be scanned) as a HeapFileScan object
+    HeapFileScan hfs(projNames[0].relName, status);
+    if (status != OK){
+        return status;
+	}
+
+	//4. check if an unconditional scan is required
+
+	if (attrDesc == NULL) {
+		status = hfs.startScan(0, 0, STRING,  NULL, EQ); // 使用 NULL 作为 filter，并设定 op 为 EQ
+	}
+	//5. check attrType: INTEGER, FLOAT, STRING
+	else {
+		switch (attrDesc->attrType) {
+			case STRING:
+				status = hfs.startScan(attrDesc->attrOffset, attrDesc->attrLen, STRING, filter, (Operator)op); // 使用字符串作为 filter，并将 op 转换为 Operator 类型
+				break;
+			case INTEGER: {
+				int intValue = atoi(filter);
+				status = hfs.startScan(attrDesc->attrOffset, attrDesc->attrLen, INTEGER, (char*)&intValue, (Operator)op); // 使用整数作为 filter，并将 op 转换为 Operator 类型
+				break;
+			}
+			case FLOAT: {
+				float floatValue = atof(filter);
+				status = hfs.startScan(attrDesc->attrOffset, attrDesc->attrLen, FLOAT, (char*)&floatValue, (Operator)op); // 使用浮点数作为 filter，并将 op 转换为 Operator 类型
+				break;
+			}
+		}
+	}
+
+	// 6. scan the current table
+	if (status != OK) {
+		return status;
+	}
+
+
+
+	// 7. Use while loop to search, if find a record, then copy stuff over to the temporary record
+	RID rid;
+    Record rec;
+    Record resultRec;
+    resultRec.data = (void *)outputData;
+    resultRec.length = reclen;
+	RID outRid;
+	while (hfs.scanNext(rid) == OK){
+		// Get the record
+		if ((status = hfs.getRecord(rec)) != OK){
+			return status;
+		}
+
+
+		// Create a new record from the projection
+		int outputOffset = 0;
+		for (int i = 0; i < projCnt; i++){
+			memcpy(outputData + outputOffset, (char *)rec.data + projNames[i].attrOffset, projNames[i].attrLen);
+			outputOffset += projNames[i].attrLen;
+		}
+
+		// Store the record in the result file
+		if ((status = resultFile.insertRecord(resultRec, outRid)) != OK){
+			return status;
+		}
+	}
+
+	// Return OK if the loop completes successfully
+    return OK;
+}
 	}
 
 	cout << "Scan finished" << endl; // debugging
